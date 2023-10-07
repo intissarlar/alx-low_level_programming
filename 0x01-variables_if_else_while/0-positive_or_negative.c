c6c1b194ceea1c872fd9835128acb360154cc5c3
intissarlaraji <intilaraji@gmail.com>
2023-10-06 18:48:22 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,82 @@
+/*
+* File: 0-positive_or_negative.c
+* Auth: intissarlaraji
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - Enty point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (success)
+ */ 
+
 
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0)); // Seed the random number generator
+	srand(time(0));
+        n= rand() - RAND_MAX / 2;
+        if (n > 0)
+                printf("ad is positive\n", n);
+        else if (n< 0)
+                printf("ed is negative\n", n);
+        else
+                printf("\d is zero\n", n);
 
-    n = rand() - RAND_MAX / 2; // Generate a random number
+        return (0);
 
-    printf(« %d is « , n);
 
-    if (n > 0)
-    {
-        printf(« positive\n »);
-    }
-    else if (n < 0)
-    {
-        printf(« negative\n »);
-    }
-    else
-    {
-        printf(« zero\n »);
-    }
 
-    return (0);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
 
