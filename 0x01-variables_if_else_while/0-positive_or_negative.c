58d52b98410961c92005f8005bc85396a1c4a59d
intissarlaraji <intilaraji@gmail.com>
2023-10-06 17:49:16 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int n;
+
+    srand(time(0)); // Seed the random number generator
+
+    n = rand() - RAND_MAX / 2; // Generate a random number
+
+    printf(« %d is « , n);
+
+    if (n > 0)
+    {
+        printf(« positive\n »);
+    }
+    else if (n < 0)
+    {
+        printf(« negative\n »);
+    }
+    else
+    {
+        printf(« zero\n »);
+    }
+
+    return (0);
+}
+
